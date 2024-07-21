47bebff54eacad5a6aa865df30521090c61bdab6
ArmanSingh-1 <156684173+ArmanSingh-1@users.noreply.github.com>
2024-07-21 11:02:06 +0000

    Binary to Decimal Convertion

--- Convertions/binary_to_decimal.py
+++ Convertions/binary_to_decimal.py
@@ -1,13 +1,32 @@
-n = (input("Enter the Binary Number: "))
+#Take binary input
+n = (input("Enter the Binary Number: \n"))
 
+#Check if input is a number
+if (not(n.isdigit())):
+    print("Invalid Entry.")
+    exit()
+
+#checks if input is in binary form
+for i in n:
+    if (i not in "01"):
+        print("Invalid Number.")
+        exit()
+
+#convertion Function    
 def binary_to_decimal(n):
-    list1 = []
-    decimal = 0
+    list1 = []                          #empty list
+    decimal = 0                         #decimal number init 
+    
+    #string convert to list
     for i in n:
         list1.append(int(i))
-    list1.reverse()
+
+    list1.reverse()                     #reversing list for easy calculation
+    
+    #Multiplying with 2^n and adding it
     for i in range(len(list1)):        
         decimal = decimal + ((2**(i))*(list1[i]))
+    
     return decimal
 
-print(binary_to_decimal(n))
+print(f"The Decimal representation of binary {n} is {binary_to_decimal(n)}.")
